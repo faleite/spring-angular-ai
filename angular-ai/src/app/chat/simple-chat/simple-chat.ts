98ade0db3867fb4c56329adad77ccbadfc33eff2
faleite <fabricio_2310@hotmail.com>
2025-09-21 15:49:51 +0100

    (Update) Created a simple-chat idea

---
+++ angular-ai/src/app/chat/simple-chat/simple-chat.ts
@@ -0,0 +1,47 @@
+import { Component, signal } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { MatToolbar } from "@angular/material/toolbar";
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { NgClass } from "@angular/common";
+
+@Component({
+  selector: 'app-simple-chat',
+  imports: [MatCardModule, MatToolbar, MatInputModule, MatButtonModule, MatIconModule, FormsModule, NgClass],
+  templateUrl: './simple-chat.html',
+  styleUrl: './simple-chat.scss'
+})
+export class SimpleChat {
+
+  userInput = '';
+
+  messages = signal([
+    { text: 'Hello, how can I help you today?', isBot: true}
+  ]);
+
+  sendMessage(){
+    this.trimUserMessage();
+    if(this.userInput != ''){
+      this.updateMessages(this.userInput);
+      this.userInput = '';
+      this.simulateResponse();
+    }
+  }
+
+  private updateMessages(text: string, isBot = false){
+    this.messages.update(messages => [...messages, { text, isBot}]);
+  }
+
+  private trimUserMessage(){
+    this.userInput = this.userInput.trim();
+  }
+
+  private simulateResponse(){
+    setTimeout(() => {
+      const response = 'This is a simulated response from Chat AI.';
+      this.updateMessages(response, true);
+    }, 2000);
+  }
+}
