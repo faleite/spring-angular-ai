c8a0004dd94aacfce49b5d9d46c61a99084328e9
faleite <fabricio_2310@hotmail.com>
2025-09-22 22:48:13 +0100

    Add MySQL-backed chat memory system and REST API for memory-based chats
    Introduces `MemoryChatService` and `MemoryChatController` for handling memory-based chat interactions. Implements MySQL schema for chat memory storage and updates Docker Compose to include MySQL service.

--- api-ai/src/main/java/com/faleite/api_ai/memory/MemoryChatController.java
+++ api-ai/src/main/java/com/faleite/api_ai/memory/MemoryChatController.java
@@ -1,4 +1,24 @@
 package com.faleite.api_ai.memory;
 
+import com.faleite.api_ai.chat.ChatMessage;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chat-memory")
 public class MemoryChatController {
+
+    private final MemoryChatService chatService;
+
+    public MemoryChatController(MemoryChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @PostMapping
+    ChatMessage simpleChat(@RequestBody ChatMessage message) {
+        String response = chatService.simpleChat(message.message());
+        return new ChatMessage(response);
+    }
 }
--- api-ai/src/main/java/com/faleite/api_ai/memory/MemoryChatService.java
+++ api-ai/src/main/java/com/faleite/api_ai/memory/MemoryChatService.java
@@ -1,4 +1,36 @@
 package com.faleite.api_ai.memory;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemoryChatService {
+
+    private final ChatClient chatClient;
+
+    public MemoryChatService(ChatClient.Builder chatClientBuilder, JdbcChatMemoryRepository jdbcChatMemoryRepository) {
+
+        ChatMemory chatMemory = MessageWindowChatMemory.builder()
+                .chatMemoryRepository(jdbcChatMemoryRepository)
+                .maxMessages(10)
+                .build();
+
+        this.chatClient = chatClientBuilder
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).build(),
+                new SimpleLoggerAdvisor())
+                .build();
+    }
+
+    public String simpleChat(String message) {
+        return this.chatClient.prompt()
+//                .advisors( a -> a.param(ChatMemory.CONVERSATION_ID, "123456"))
+                .user(message)
+                .call()
+                .content();
+    }
 }
