f6f5eb3f5c629f511333d909ba2ca2e6bcdddd5d
faleite <fabricio_2310@hotmail.com>
2025-09-25 20:42:24 +0100

    Remove unused `ChatMemoryRepository` import from `MemoryChatService`

--- api-ai/src/main/java/com/faleite/api_ai/memory/MemoryChatService.java
+++ api-ai/src/main/java/com/faleite/api_ai/memory/MemoryChatService.java
@@ -4,7 +4,6 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
-import org.springframework.ai.chat.memory.ChatMemoryRepository;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
 import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.stereotype.Service;
